646fe8f7e40e1ebd2e26bf5c774a58e49355a7a9
Nashwin-Dsouza <nashwindsouza.ise@skit.org.in>
2025-11-02 20:22:32 +0530

    fix:mail logic

--- src/lib/sendEmail.js
+++ src/lib/sendEmail.js
@@ -3,7 +3,9 @@ import nodemailer from "nodemailer";
 
 // Set up the transporter one time
 const transporter = nodemailer.createTransport({
-  service: "gmail", // Or your email provider
+  host: "smtp.gmail.com",
+  port: 465, // This is the SSL port
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
